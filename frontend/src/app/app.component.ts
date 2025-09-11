07beb1686feaf97a2bc193ad361f67fab9a6fa87
acepect0 <rubencardosogaspar@gmail.com>
2025-09-11 18:30:46 +0100

    feat: Added footer to the main page.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MainmessageComponent } from './mainmessage/mainmessage.component';
 import { TopshopComponent } from './topshop/topshop.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [NavbarComponent, MainmessageComponent, TopshopComponent]
+  imports: [NavbarComponent, MainmessageComponent, TopshopComponent, FooterComponent]
 })
 export class AppComponent {}
